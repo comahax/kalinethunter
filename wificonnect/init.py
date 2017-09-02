582093eedda4fbf8c2ebfa60d8d969cee5a28ef0
comahax <123654dagao@sina.cn>
2017-09-02 15:22:24 +0800

    wpa_passphrase

--- wificonnect/init.py
+++ wificonnect/init.py
@@ -1,6 +1,6 @@
 #coding:utf-8
 import os
-
+wifilist = []
 def runcmd(cmd):
     cmd = cmd
     output = os.popen(cmd)
@@ -10,8 +10,10 @@ def runcmd1(cmd):
     cmd = cmd
     output = os.popen(cmd)
     for l in output.readlines():
+        number = 0
         if 'ESSID' in l:
-            print l
+            print str(number)+":"+l
+            wifilist.append(l.split(':')[1])
 
 
 if __name__ == '__main__':
@@ -21,4 +23,14 @@ if __name__ == '__main__':
     print '搜索附近wifi'
     cmd = 'iwlist wlan1 scan'
     runcmd1(cmd)
+    print '输入你要连接的wifi编号'
+    a = raw_input('please input the wifi number which you want to connect')
+    a = int(a)
+    cmd = 'wpa_passphrase %s > /etc/wpa_supplicant.conf' % (wifilist[a])
+    print cmd
+    runcmd(cmd)
+    cmd = 'wpa_supplicant -i wlan1 -c /etc/wpa_supplicant.conf'
+    runcmd(cmd)
+
+
 
